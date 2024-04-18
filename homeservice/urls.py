7ef0957da1d0fb1f26e1d00c3c21559790390788
Manish Bhandari <mac@Macs-MacBook-Pro.local>
2024-04-18 20:07:03 +0545

    first commit/login

---
+++ homeservice/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from homeservice import views
+
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("home", views.home, name="home"),
+    path("about", views.about, name="about"),
+    path("service", views.service, name="service"),
+    path("contact", views.contact, name="contact"),
+
+    # Authentication
+    path("login", views.login_page, name="login"),
+]
---
+++ homeservice/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, HttpResponse
+
+
+# Create your views here.
+def home(request):
+    return render(request, "index.html")
+
+
+def about(request):
+    return render(request, "about.html")
+
+
+def service(request):
+    return render(request, "services.html")
+
+
+def contact(request):
+    return render(request, "contact.html")
+
+
+def login_page(request):
+    return render(request, "login.html")
