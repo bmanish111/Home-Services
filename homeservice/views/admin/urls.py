0c3de243f0ef72d47fe69b4784776a7620637408
Manish Bhandari <mac@Macs-MacBook-Pro.local>
2024-04-22 12:19:30 +0545

    edit del employee

--- homeservice/views/admin/urls.py
+++ homeservice/views/admin/urls.py
@@ -7,7 +7,19 @@ urlpatterns = [
     path("", views.home, name="admin_home"),
     path("employee/create", views.employee, name="employee_create"),
     path("employee", views.employee_list, name="employee"),
-    path("employee/application", views.employee_application, name="employee_application"),
-    path("employee/application/<int:employee_id>", views.employee_application, name="employee_application"),
-    path("employee/application/<int:employee_id>/verify", views.employee_verification, name="employee_verification"),
+    path("employee/<int:employee_id>/edit", views.employee_edit, name="employee_edit"),
+    path("employee/<int:employee_id>/delete", views.employee_delete, name="employee_delete"),
+    path(
+        "employee/application", views.employee_application, name="employee_application"
+    ),
+    path(
+        "employee/application/<int:employee_id>",
+        views.employee_application,
+        name="employee_application",
+    ),
+    path(
+        "employee/application/<int:employee_id>/verify",
+        views.employee_verification,
+        name="employee_verification",
+    ),
 ]
